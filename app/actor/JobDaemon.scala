8b15b8298b35c425948ba9e682dd51ded8b1ae49
basileduplessis <basile.duplessis@gmail.com>
2014-09-25 13:19:38 +0200

    Cron to activate cloudbees

--- app/actor/JobDaemon.scala
+++ app/actor/JobDaemon.scala
@@ -4,6 +4,7 @@ package actor
 import akka.actor.Actor
 import play.api.Logger
 import services.BoncoinService
+import scala.io.Source
 
 
 /**
@@ -12,9 +13,13 @@ import services.BoncoinService
 class JobDaemon extends Actor {
 
   def receive = {
-    case _ => {
+    case "jobActor" => {
       Logger.info("Looking for jobs to execute")
       BoncoinService.doJobs
+    }
+    case "cloudBeesActivator" => {
+      Logger.info("Call cloudbees home")
+      Source.fromURL("http://cronjobsample.basileduplessis.eu.cloudbees.net/")
     }
   }
 
