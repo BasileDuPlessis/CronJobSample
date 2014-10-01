b10f27cd563ff06c74197407203170d9609df8ac
basileduplessis <basile.duplessis@gmail.com>
2014-10-01 10:31:33 +0200

    Refactor parsing ads

--- app/services/BoncoinService.scala
+++ app/services/BoncoinService.scala
@@ -38,7 +38,8 @@ object BoncoinService {
     con.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.1.2) Gecko/20090729 Firefox/3.5.2 (.NET CLR 3.5.30729)")
     con.connect()
 
-    Logger.info("Time out: " + con.getContent.asInstanceOf[String])
+    Logger.info("Code response: " + con.getResponseCode)
+    Logger.info("Response message: " + con.getResponseMessage)
 
     ""
   }
