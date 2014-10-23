fffa11c133c5b94db40137bef5460ded3f8ad3b4
basileduplessis <basile.duplessis@gmail.com>
2014-10-23 18:32:53 +0200

    Functionnal test for Jobs Tasks

--- test/JobExecutionServiceSpec.scala
+++ test/JobExecutionServiceSpec.scala
@@ -1,8 +1,13 @@
+import java.io.InputStream
+
 import com.github.athieriot.EmbedConnection
+import controllers.Jobs
 import models.Job
 import org.junit.runner.RunWith
 import org.specs2.mutable.Specification
 import org.specs2.runner.JUnitRunner
+import org.subethamail.smtp.helper.{SimpleMessageListenerAdapter, SimpleMessageListener}
+import org.subethamail.smtp.server.SMTPServer
 import play.api.test.FakeApplication
 import org.specs2.mutable.{ BeforeAfter, Specification }
 import play.api.test.Helpers._
@@ -15,8 +20,9 @@ import com.github.tomakehurst.wiremock.client.WireMock
 import com.github.tomakehurst.wiremock.client.WireMock._
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration._
 
-import scala.concurrent.Await
+import scala.concurrent.{Promise, Await}
 import scala.concurrent.duration.Duration
+import scala.io.Source
 
 @RunWith(classOf[JUnitRunner])
 /**
@@ -25,6 +31,19 @@ import scala.concurrent.duration.Duration
 class JobExecutionServiceSpec extends Specification with EmbedConnection {
   sequential
 
+  val promiseMessage = Promise[String]
+  val lastReceivedMessage = promiseMessage.future
+
+  val messageListener = new SimpleMessageListener {
+    def accept(from: String, recipient: String): Boolean = true
+    def deliver(from: String, recipient: String, data: InputStream): Unit = {
+      if (!promiseMessage.isCompleted) promiseMessage.success(Source.fromInputStream(data).mkString)
+    }
+  }
+
+  val smtpServer = new SMTPServer(new SimpleMessageListenerAdapter(messageListener))
+  smtpServer.start()
+
   val Port = 8080
   val Host = "localhost"
 
@@ -32,27 +51,51 @@ class JobExecutionServiceSpec extends Specification with EmbedConnection {
   wireMockServer.start()
   WireMock.configureFor(Host, Port)
 
-  val conf = testConfConnection
+  val conf =  Map("smtp.host" -> smtpServer.getHostName) ++ testConfConnection
 
   val fakeApp = FakeApplication(additionalConfiguration = conf)
 
   "JobExecutionService#executeAll" should {
-    "update Ads and send Email" in running(fakeApp) {
-      stubFor(
-        get(urlEqualTo("/"))
-          .willReturn(
-            aResponse()
-              .withBody("body")
-          )
-      )
+    "update Ads and send Email" in context {
+      running(fakeApp) {
 
-      val job = Job(Some(BSONObjectID.generate), "url", None)
+        val content = """
+          http://www.leboncoin.fr/ventes_immobilieres/0123456789.htm
+          http://www.leboncoin.fr/ventes_immobilieres/9876543210.htm
+        """
 
-      Await.result(Job.insert(job)(connection), Duration.Inf)
+        val contentMatchA = "http://www.leboncoin.fr/ventes_immobilieres/0123456789.htm"
+        val contentMatchB = "http://www.leboncoin.fr/ventes_immobilieres/9876543210.htm"
 
-      scala.io.Source.fromURL("http://localhost:8080/").getLines.mkString must beEqualTo("body")
+        stubFor(
+          get(urlEqualTo("/"))
+            .willReturn(
+              aResponse()
+                .withBody(content)
+            )
+        )
+
+        val job = Job(None, "http://localhost:8080/", None)
+
+        Await.result(Job.insert(job)(connection), Duration.Inf)
+
+        Await.result(Jobs.executeAllJobs, Duration.Inf)
+
+        Await.result(lastReceivedMessage, Duration.Inf) must contain(contentMatchA)
+        Await.result(lastReceivedMessage, Duration.Inf) must contain(contentMatchB)
+
+      }
     }
   }
 
+  object context extends BeforeAfter {
+
+    def before = {
+    }
+
+    def after = {
+    }
+
+  }
 
 }
